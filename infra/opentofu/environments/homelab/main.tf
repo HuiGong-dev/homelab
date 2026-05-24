66f665218c00068a3508823223ba17d7247b2551
Hui Gong <whygonghui@gmail.com>
2026-05-24 16:08:43 +0200

    feat(tofu): add k3s-server-01 vm

--- infra/opentofu/environments/homelab/main.tf
+++ infra/opentofu/environments/homelab/main.tf
@@ -1,5 +1,6 @@
 locals {
-  adguard_ip_config = "192.168.178.12/24"
+  adguard_ip_config       = "192.168.178.12/24"
+  k3s_server_01_ip_config = "192.168.178.13/24"
 }
 
 module "adguard" {
@@ -20,6 +21,33 @@ module "adguard" {
   disk_size_gb = 10
 
   ip_config    = local.adguard_ip_config
+  ipv4_gateway = "192.168.178.1"
+
+  ci_user          = "ansible"
+  ci_user_password = var.ci_user_password
+  ssh_public_keys  = [var.ssh_public_key_automation]
+  started          = true
+  stop_on_destroy  = true
+}
+
+module "k3s-server-01" {
+  source = "../../modules/proxmox-vm"
+
+  vm_name        = "k3s-server-01"
+  description    = "Managed by OpenTofu"
+  tags           = ["opentofu", "ansible", "k3s-server"]
+  node_name      = var.proxmox_node_name
+  vm_id          = 104
+  template_vm_id = var.template_vm_id
+  full_clone     = true
+
+  cpu_cores    = 4
+  memory_mb    = 12288
+  datastore_id = "local-lvm"
+  bridge       = "vmbr0"
+  disk_size_gb = 250
+
+  ip_config    = local.k3s_server_01_ip_config
   ipv4_gateway = "192.168.178.1"
 
   ci_user          = "ansible"
--- infra/opentofu/environments/homelab/output.tf
+++ infra/opentofu/environments/homelab/output.tf
@@ -9,3 +9,15 @@ output "adguard_vm_id" {
 output "adguard_vm_name" {
   value = module.adguard.name
 }
+
+output "k3s_server_01_vm_ip" {
+  value = module.k3s-server-01.cloud_init_ipv4_address
+}
+
+output "k3s_server_01_vm_id" {
+  value = module.k3s-server-01.vm_id
+}
+
+output "k3s_server_01_vm_name" {
+  value = module.k3s-server-01.name
+}
