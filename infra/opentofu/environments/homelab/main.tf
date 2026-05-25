ad2f68fb3a9193b7980b08b9a2300b42e2a1f63c
Hui Gong <whygonghui@gmail.com>
2026-05-25 10:18:41 +0200

    feat(tofu): add k3s-agent-01

--- infra/opentofu/environments/homelab/main.tf
+++ infra/opentofu/environments/homelab/main.tf
@@ -1,6 +1,7 @@
 locals {
   adguard_ip_config       = "192.168.178.12/24"
   k3s_server_01_ip_config = "192.168.178.13/24"
+  k3s_agent_01_ip_config  = "192.168.178.14/24"
 }
 
 module "adguard" {
@@ -35,7 +36,7 @@ module "k3s-server-01" {
 
   vm_name        = "k3s-server-01"
   description    = "Managed by OpenTofu"
-  tags           = ["opentofu", "ansible", "k3s-server"]
+  tags           = ["opentofu", "ansible", "k3s", "k3s-server"]
   node_name      = var.proxmox_node_name
   vm_id          = 104
   template_vm_id = var.template_vm_id
@@ -48,6 +49,33 @@ module "k3s-server-01" {
   disk_size_gb = 250
 
   ip_config    = local.k3s_server_01_ip_config
+  ipv4_gateway = "192.168.178.1"
+
+  ci_user          = "ansible"
+  ci_user_password = var.ci_user_password
+  ssh_public_keys  = [var.ssh_public_key_automation]
+  started          = true
+  stop_on_destroy  = true
+}
+
+module "k3s-agent-01" {
+  source = "../../modules/proxmox-vm"
+
+  vm_name        = "k3s-agent-01"
+  description    = "Managed by OpenTofu"
+  tags           = ["opentofu", "ansible", "k3s", "k3s-agent"]
+  node_name      = var.proxmox_node_name
+  vm_id          = 105
+  template_vm_id = var.template_vm_id
+  full_clone     = true
+
+  cpu_cores    = 2
+  memory_mb    = 4096
+  datastore_id = "local-lvm"
+  bridge       = "vmbr0"
+  disk_size_gb = 50
+
+  ip_config    = local.k3s_agent_01_ip_config
   ipv4_gateway = "192.168.178.1"
 
   ci_user          = "ansible"
